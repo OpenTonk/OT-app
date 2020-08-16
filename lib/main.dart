4cb1d2d6c7014bfb99c693f15f0ac0dbe08225f4
neotje <neotje111@gmail.com>
2020-08-16 19:46:53 +0200

    added connect button added connecting logic added android permission added dependecies

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:wifi_configuration/wifi_configuration.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-void main() {
+import 'dart:io';
+import 'dart:convert';
+import 'dart:async';
+
+FlutterLocalNotificationsPlugin notificationsPlugin = FlutterLocalNotificationsPlugin();
+var androidSettings = AndroidInitializationSettings('');
+var iosSettings = IOSInitializationSettings();
+var settings = InitializationSettings(androidSettings, iosSettings);
+
+var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+    'OT-n-1', 'OT-app', 'OpenTank notifications',
+    importance: Importance.Max, priority: Priority.High, ticker: 'ticker');
+var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+var platformChannelSpecifics = NotificationDetails(
+    androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+
+void main() async {
+  notificationsPlugin.initialize(settings);
   runApp(MyApp());
 }
 
@@ -9,109 +28,87 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'TonkConnect',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
+        primarySwatch: Colors.grey,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MainPage(title: ''),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class MainPage extends StatefulWidget {
+  MainPage({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MainPage createState() => _MainPage();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MainPage extends State<MainPage> {
+  String ssid = "OpenTank";
+  String pass = "UltimateTonk";
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  String host = "192.168.4.1";
+  int port = 4200;
+
+  bool isConnected = false;
+  Socket socket;
+
+  void onConnectBtn() async {
+    if (!this.isConnected) {
+      WifiConnectionStatus connStatus = await WifiConfiguration.connectToWifi(
+          this.ssid, this.pass, "com.neo.ot_app");
+
+      switch (connStatus) {
+        case WifiConnectionStatus.connected:
+          this.isConnected = true;
+          break;
+
+        case WifiConnectionStatus.alreadyConnected:
+          this.isConnected = true;
+          break;
+
+        case WifiConnectionStatus.notConnected:
+          this.isConnected = false;
+          break;
+
+        case WifiConnectionStatus.platformNotSupported:
+          print("platformNotSupported");
+          break;
+
+        case WifiConnectionStatus.profileAlreadyInstalled:
+          print("profileAlreadyInstalled");
+          break;
+
+        case WifiConnectionStatus.locationNotAllowed:
+          print("locationNotAllowed");
+          break;
+      }
+
+      if(this.isConnected) {
+        this.socket = await Socket.connect(this.host, this.port);
+        print('connected');
+      } else {
+        await notificationsPlugin.show(0, 'Failed to connect', 'Failed to find your OpenTank hotspot', platformChannelSpecifics);
+      }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
+      body: Center(),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: onConnectBtn,
+        tooltip: 'Connect',
+        child: Icon(Icons.wifi),
+      ),
     );
   }
 }
